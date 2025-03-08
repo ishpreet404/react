9c26a1749b809aad545b5d975d5355ea770f185b
Ishpreet Singh <ishpreet@outlook.in>
2025-03-09 01:24:15 +0530

    dom tree

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-const heading = React.createElement("h1",{id:"h"},"Hello World using react");
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(heading);
+const head =React.createElement("h1",{id:"h"},"I am h1");
+const child =React.createElement("div",{id:"child"},head);
+const parent = React.createElement("div",{id:"parent"},child);
+const root =ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
