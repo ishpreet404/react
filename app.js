3a82f020f7a3a601efff9e526b4ef76129362e71
Ishpreet Singh <ishpreet@outlook.in>
2025-03-09 03:19:10 +0530

    parent_child

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const head =React.createElement("h1",{id:"h"},"I am h1");
+const head =[React.createElement("h1",{id:"h"},"I am h1"),React.createElement("h2",{id:"h"},"I am h2")]; //solution JSX
 const child =React.createElement("div",{id:"child"},head);
 const parent = React.createElement("div",{id:"parent"},child);
-const root =ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const root =ReactDOM.createRoot(document.getElementById("root")); 
+root.render(parent); //react is only working in the root div 
+// react element is a object that browser understands
