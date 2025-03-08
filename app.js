d4613c0866bf01b9fac6043d62342c20c510ed6e
Ishpreet Singh <ishpreet@outlook.in>
2025-03-08 19:51:11 +0530

    commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1",{id:"h"},"Hello World using react");
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(heading);
