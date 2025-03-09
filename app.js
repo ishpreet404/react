76f41cd09ebaf69314fd5158aff64e78fb9796d7
Ishpreet Singh <ishpreet@outlook.in>
2025-03-09 21:43:51 +0530

    notes

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const head =[React.createElement("h1",{id:"h"},"I am h1"),React.createElement("h2",{id:"h"},"I am h2")]; //solution JSX
 const child =React.createElement("div",{id:"child"},head);
 const parent = React.createElement("div",{id:"parent"},child);
 const root =ReactDOM.createRoot(document.getElementById("root")); 
 root.render(parent); //react is only working in the root div 
-// react element is a object that browser understands
-//two types of dependecies dev and normal -D => dev dependecy 
-// ^ caret or ~ tilde will automatically upgrade the version ^ minor update ~ major update 
-//package lock keeps the track via hash on local and production 
-//transative dependecies => node modules
---
+++ notes.md
@@ -0,0 +1,11 @@
+// react element is a object that browser understands
+//two types of dependecies dev and normal -D => dev dependecy 
+// ^ caret or ~ tilde will automatically upgrade the version ^ minor update ~ major update 
+//package lock keeps the track via hash on local and production 
+//transative dependecies => node modules
+we can regenerate the node modules with package.json and package.lock.json  
+if we want to execute a package we use npx and if we instal a package we use npm 
+fetching from cdn is not a goodway to use react 
+parcel is doing hmr (hot module replacement) 
+it uses a file watching algorithm written in c++
+parcel is caching things up and reduces the build time
