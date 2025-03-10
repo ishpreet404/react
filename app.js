22818f6294971fe8cc9adf4e43d9c3f12f427f08
Ishpreet Singh <ishpreet@outlook.in>
2025-03-11 01:47:55 +0530

    component composing

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 //JSX is not html in js it is a html like syntax 
-const jsxheading = <h1>This is a jsx heading</h1>
-const root = ReactDOM.createRoot(document.getElementById("root")); // parcel transpiles this code and browser is unable to understand the code as it runs on ECMAscript6
-root.render(jsxheading);
+const jsxheading = <h1 className="root" tabIndex="5"> This is a jsx heading</h1>
+const root = ReactDOM.createRoot(document.getElementById("root")); // parcel transpiles this code and browser is unable to understand the code as it runs on ECMAscript6 using babel
+//React Components 
+//Classbases components -OLD
+//Functional components -NEW
+const Title = ()=> (
+    <h1>parsing one compenent block into another</h1>
+);
+const nm = 100;
+const Headingcompenent = ()=> ( // component composition    
+    <div>    
+    <Title/>  
+    {Title()}
+    <Title></Title>
+    {jsxheading} 
+    {/* if a malacious api call is made jsx prevents injection attacks  */}
+    {nm}
+     <h2>functional compenent </h2>
+    </div>
+)
+root.render(<Headingcompenent/>);
 
