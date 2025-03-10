cd13d4cd2f9eb4e70ada853d3f5c44256232a212
Ishpreet Singh <ishpreet@outlook.in>
2025-03-10 23:23:14 +0530

    jsx

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const head =[React.createElement("h1",{id:"h"},"I am h1"),React.createElement("h2",{id:"h"},"I am h2")]; //solution JSX
-const child =React.createElement("div",{id:"child"},head);
-const parent = React.createElement("div",{id:"parent"},child);
-const root =ReactDOM.createRoot(document.getElementById("root")); 
-root.render(parent); //react is only working in the root div 
+//JSX is not html in js it is a html like syntax 
+const jsxheading = <h1>This is a jsx heading</h1>
+const root = ReactDOM.createRoot(document.getElementById("root")); // parcel transpiles this code and browser is unable to understand the code as it runs on ECMAscript6
+root.render(jsxheading);
+
--- temp.js
+++ temp.js
@@ -1,4 +1,9 @@
-const heading = document.createElement("h1");
-heading.innerHTML="Hello world from javascript";
-const root = document.getElementById("root");
-root.appendChild(heading);
+// const heading = document.createElement("h1");
+// heading.innerHTML="Hello world from javascript";
+// const root = document.getElementById("root");
+// root.appendChild(heading);
+// const head =[React.createElement("h1",{id:"h"},"I am h1"),React.createElement("h2",{id:"h"},"I am h2")]; //solution JSX
+// const child =React.createElement("div",{id:"child"},head);
+// const parent = React.createElement("div",{id:"parent"},child);
+// const root =ReactDOM.createRoot(document.getElementById("root")); 
+// root.render(parent); //react is only working in the root div 
